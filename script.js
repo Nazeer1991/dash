5674cc39a886193996a1cad804b521dbffc21dfb
Nazeer <68148485+Nazeer1991@users.noreply.github.com>
2022-03-21 22:51:41 +0530

    added array methods

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const data = [
     year: 2000,
   },
 ];
+const array = ["bro", "sis", "kid"];
 
 let manufacturer = document.querySelector(".manufacturer");
 let model = document.querySelector(".model");
@@ -25,5 +26,110 @@ model.innerHTML = data[0].model;
 
 let button = document.querySelector("button");
 button.onclick = () => {
-  year.innerHTML = data[0].year;
+  year.innerHTML = array;
 };
+
+const circles = document.querySelectorAll(".circle");
+
+circles.forEach((circle) => (circle.style.backgroundColor = "green"));
+/// object.map
+const housemates = [
+  {
+    name: "ahmend",
+    age: 24,
+  },
+  {
+    name: "ellen",
+    age: 35,
+  },
+  {
+    name: "woody",
+    age: 22,
+  },
+];
+
+const housemates2021 = housemates.map((housemate) => housemate.age + 2);
+console.log(housemates2021);
+/// filter array
+const shows = ["mr.robot", "lost", "dexter", "got", "breaking bad"];
+
+console.log(
+  shows.filter((show) => {
+    return show.length < 5;
+  })
+);
+// filter object
+const objShows = [
+  { name: "lupin", series: 1, episode: 3 },
+  { name: "got", series: 2, episode: 22 },
+  { name: "bad", series: 3, episode: 5 },
+  { name: "robot", series: 1, episode: 9 },
+];
+
+console.log(objShows.filter((each) => each.episode < 10));
+//reduce()
+
+const scores = [23, 21, 44, 55, 53];
+
+//scores.reduce((accumulator,currentValue) => accumulator + currentValue)
+
+const total = scores.reduce((totalScore, score) => totalScore + score);
+
+console.log(total);
+
+//every()
+//if everything in our array matches a rule return true
+
+const testResults = [45, 34, 76, 89, 45, 65, 76];
+
+const hasClassHasPassed = testResults.every((result) => result > 30);
+console.log(hasClassHasPassed);
+
+const animal = ["cat", "dog", "rat"];
+
+const everyAnimalHasThreeCharacter = animal.every((an) => an.length === 3);
+
+console.log(everyAnimalHasThreeCharacter);
+
+/// Array Work
+
+/// using 'map' fn converts farnheit to celcius
+let farnheit = [120, 140, 212, 200];
+
+function getCelcius(arr) {
+  return arr.map((temp) => (temp - 32) * (5 / 9));
+}
+
+const celcius = getCelcius(farnheit);
+
+console.log(celcius);
+
+// Using Some check array for falsey
+
+const checkFalsy = [11, NaN, [], "angels"];
+
+function checkForFalsy() {
+  return checkFalsy.some(
+    (elem) =>
+      elem == false ||
+      elem == 0 ||
+      elem == -0 ||
+      elem == 0n ||
+      elem == "" ||
+      elem == null ||
+      elem == undefined ||
+      elem == NaN
+  );
+}
+
+console.log(checkForFalsy());
+
+// reduce add chars
+const getTotals = ["rabbit", "football", "cracking"];
+
+function getTotal() {
+  return getTotals.reduce((total, item) => total + item.length, 0);
+}
+console.log(getTotal());
+
+//
